affe913df93e8b92f2172680de1a45f1d48a0e79
sudheer <sudheerlikeu@gmail.com>
2022-03-06 11:38:17 +0530

    --- Auto-Git Commit ---

--- classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
+++ classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
@@ -16,7 +16,7 @@ NOTE:  version constrains
         version updates happens like ~> "1.1.5" allows "1.1.6" "1.1.7" "1.1.8"
         but it will                             deny   "1.2.0" "1.2.1" "1.2.2"
         because  the ~> symbole will allow you only right most version upgrades 
-        it will not allow the middle numer hear
+        it will not allow the middle numer hear "1.2.0"
 
 
 */
