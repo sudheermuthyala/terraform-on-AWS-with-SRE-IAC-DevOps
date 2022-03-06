5f5f16095d18363a911fe8e1335c41591a01c0bb
sudheer <sudheerlikeu@gmail.com>
2022-03-06 11:37:27 +0530

    --- Auto-Git Commit ---

--- classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
+++ classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
@@ -15,7 +15,7 @@ NOTE:  version constrains
         "~>" what does it's do is like any chainge to rightt most 
         version updates happens like ~> "1.1.5" allows "1.1.6" "1.1.7" "1.1.8"
         but it will                             deny   "1.2.0" "1.2.1" "1.2.2"
-        because  the ~> symbole will allow you to 
+        because  the ~> symbole will allow you only right most
 
 
 */
