38f619a6a35489340c966720b811d6e97a9044e0
sudheer <sudheerlikeu@gmail.com>
2022-03-06 11:34:36 +0530

    --- Auto-Git Commit ---

--- classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
+++ classes/02-terraform-basics/18-terraform-block/terraform-manefist/c1-version.tf
@@ -14,6 +14,7 @@ NOTE:  version constrains
         required_version = "~> 1.1"  
         "~>" what does it's do is like any chainge to rightt most 
         version updates happens like ~> "1.1.5" allows "1.1.6" "1.1.7" "1.1.8"
+        
 
 
 */
