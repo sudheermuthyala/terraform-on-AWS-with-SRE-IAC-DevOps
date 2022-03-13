a060622deb2ccafa8c8a720b1d3162dc757bdfa8
sudheer <sudheerlikeu@gmail.com>
2022-03-13 17:01:07 +0530

    --- Auto-Git Commit ---

--- classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
+++ classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
@@ -4,18 +4,18 @@ resource "aws_instance" "myec2instane" {
  instance_type          = "t2.micro"
  #user_data              =  file("app1-install.sh")
  key_name               =  "terraform-k"
- user_data = <<-EOF
-#! /bin/bash
-# Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl enable httpd
-sudo service httpd start  
-sudo echo '<h1>Welcome to Muthyala Sudheer Naidu Terraform - APP-1</h1>' | sudo tee /var/www/html/index.html
-sudo mkdir /var/www/html/app1
-sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Muthyala Sudheer Naidu Terraform- APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
-sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
-EOF
+// user_data = <<-EOF
+//#! /bin/bash
+//# Instance Identity Metadata Reference - https://docs.aws.amazon.com/AWSEC2/latest/UserGuide/instance-identity-documents.html
+//sudo yum update -y
+//sudo yum install -y httpd
+//sudo systemctl enable httpd
+//sudo service httpd start  
+//sudo echo '<h1>Welcome to Muthyala Sudheer Naidu Terraform - APP-1</h1>' | sudo tee /var/www/html/index.html
+//sudo mkdir /var/www/html/app1
+//sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Muthyala Sudheer Naidu Terraform- APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
+//sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
+//EOF
  
  tags = {
    "Name" = "Ec2-Instance-Demo"
