9fa268cb0df6174dceed2047882e1b2cca143f04
sudheer <sudheerlikeu@gmail.com>
2022-03-13 08:58:33 +0530

    --- Auto-Git Commit ---

--- classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
+++ classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
@@ -1,6 +1,6 @@
 ## resource 
 resource "aws_instance" "myec2instane" {
- ami                    = "ami-0b90346fbb8e13c09"
+ ami                    = ""
  instance_type          = "t2.micro"
  #user_data              =  file("app1-install.sh")
  user_data = <<-EOF
