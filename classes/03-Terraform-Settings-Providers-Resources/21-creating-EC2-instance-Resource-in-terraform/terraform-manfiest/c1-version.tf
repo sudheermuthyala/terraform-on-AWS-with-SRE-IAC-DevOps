a11c0a27d7df4c87aaaa1ae8e99a303e15eb5205
sudheer <sudheerlikeu@gmail.com>
2022-03-13 07:51:56 +0530

    --- Auto-Git Commit ---

--- classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/c1-version.tf
+++ classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/c1-version.tf
@@ -8,7 +8,7 @@ terraform{
         }
     }    
 }
-
+*/
 terraform {
   required_version = "~> 0.14" # which means any version equal & above 0.14 like 0.15, 0.16 etc and < 1.xx
   required_providers {
