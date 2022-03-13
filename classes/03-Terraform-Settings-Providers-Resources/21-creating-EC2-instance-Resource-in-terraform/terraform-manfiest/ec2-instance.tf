ed98d07fd2b75978db9c9f33be768e3fee273c77
sudheer <sudheerlikeu@gmail.com>
2022-03-13 08:49:40 +0530

    --- Auto-Git Commit ---

--- classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
+++ classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/ec2-instance.tf
@@ -14,7 +14,7 @@ sudo echo '<h1>Welcome to Muthyala Sudheer Naidu Terraform - APP-1</h1>' | sudo 
 sudo mkdir /var/www/html/app1
 sudo echo '<!DOCTYPE html> <html> <body style="background-color:rgb(250, 210, 210);"> <h1>Muthyala Sudheer Naidu Terraform- APP-1</h1> <p>Terraform Demo</p> <p>Application Version: V1</p> </body></html>' | sudo tee /var/www/html/app1/index.html
 sudo curl http://169.254.169.254/latest/dynamic/instance-identity/document -o /var/www/html/app1/metadata.html
- EOF
+EOF
  
  tags = {
    "Name" = "Ec2-Instance-Demo"
