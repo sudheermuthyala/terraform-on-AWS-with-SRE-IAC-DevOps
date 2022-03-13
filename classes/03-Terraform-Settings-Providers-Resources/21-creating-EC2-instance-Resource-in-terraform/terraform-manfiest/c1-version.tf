55610e9a39a464adc9991dd678ab3a7e8adc8d8f
sudheer <sudheerlikeu@gmail.com>
2022-03-13 07:57:47 +0530

    --- Auto-Git Commit ---

--- classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/c1-version.tf
+++ classes/03-Terraform-Settings-Providers-Resources/21-creating-EC2-instance-Resource-in-terraform/terraform-manfiest/c1-version.tf
@@ -1,10 +1,10 @@
 ## terraform Block
-/*terraform{
+terraform{
     required_version = "~> 1.1"
     required_providers {
         aws = {
             source = "hashicrop/aws"
-            version = "~> 4.4.0"
+            version = "~> 4.4"
         }
     }    
 }
