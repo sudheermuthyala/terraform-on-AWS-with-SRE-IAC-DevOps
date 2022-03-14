59fb2c770d741fe0ac2456bab557470f5a8372b8
sudheer <sudheerlikeu@gmail.com>
2022-03-14 18:58:08 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c3-ec2-security-group.tf
+++ classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c3-ec2-security-group.tf
@@ -24,3 +24,27 @@ resource "aws_security_group" "allow_ssh" {
 }
 
 # Create Security Group - WEB-Traffic
+
+resource "aws_security_group" "allow_" {
+  name        = "allow_ssh"
+  description = "Allow DEV ssh"
+
+  ingress {
+    description      = "Allow DEV ssh"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
