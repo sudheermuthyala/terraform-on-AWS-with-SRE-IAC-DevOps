6c1283daf375e2ceaa53a675b4ebc7dbf8674c1a
sudheer <sudheerlikeu@gmail.com>
2022-03-14 18:31:33 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c2-varibles.tf
+++ classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c2-varibles.tf
@@ -16,5 +16,5 @@ variable "instance_type" {
 
 ## AWS ec2 instance Key Pair
 variable "instance_keypair" {
- description = "AWS Ec2 keypair thet need to" 
+ description = "AWS Ec2 keypair thet need to Associated with EC" 
 }
