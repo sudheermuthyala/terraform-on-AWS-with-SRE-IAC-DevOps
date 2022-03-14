cdef2e0c8ba621b2b03c76cccbc3f1a4f71206df
sudheer <sudheerlikeu@gmail.com>
2022-03-14 18:56:48 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c3-ec2-security-group.tf
+++ classes/04-Terraform-Variables-and-Datasources/28-Create-Security-Group/terraform-manifests/c3-ec2-security-group.tf
@@ -7,7 +7,7 @@ resource "aws_security_group" "allow_ssh" {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
-    cidr_blocks      = [""]
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
   }
 
