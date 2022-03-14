677ee7631b41f134984de30d3942f45a8e7eb7df
sudheer <sudheerlikeu@gmail.com>
2022-03-14 17:04:33 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/27-Implement-the-input-varibles/terraform-manifests/c1-versions.tf
+++ classes/04-Terraform-Variables-and-Datasources/27-Implement-the-input-varibles/terraform-manifests/c1-versions.tf
@@ -17,5 +17,5 @@ variable "instance_type" {
 
 ## AWS ec2 instance Key Pair
 variable "instance_keypair" {
-  description = "AWS EC2 key pair that need to associated eith"
+  description = "AWS EC2 key pair that need to associated with EC2"
 }
