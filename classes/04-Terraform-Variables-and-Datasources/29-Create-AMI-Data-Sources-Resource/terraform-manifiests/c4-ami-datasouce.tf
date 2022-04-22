82fd8ae0c18c7ffcf21b0c100afddf0476acfcb6
sudheer <sudheerlikeu@gmail.com>
2022-04-22 10:58:18 +0530

    --- Auto-Git Commit ---

---
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c4-ami-datasouce.tf
@@ -0,0 +1,19 @@
+data "aws_ami" "example" {
+  most_recent      = true
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
