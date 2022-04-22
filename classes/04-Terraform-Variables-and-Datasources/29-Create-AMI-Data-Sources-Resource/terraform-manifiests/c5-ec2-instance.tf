7604b2074a04dd0e463169c1d469edff43c3f0c4
sudheer <sudheerlikeu@gmail.com>
2022-04-22 12:19:09 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "Myec2" {
   ami           = data.aws_ami.amazonimage
   instance_type = var.instance_type
-  key_name
+  key_name      = var.
 
   tags = {
     Name = "HelloWorld"
