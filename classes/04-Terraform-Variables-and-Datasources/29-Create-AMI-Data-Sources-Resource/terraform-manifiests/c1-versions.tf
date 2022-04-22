26d2c031f0a7f474acc575d0eb8d30b928b91737
sudheer <sudheerlikeu@gmail.com>
2022-04-22 10:44:29 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c1-versions.tf
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c1-versions.tf
@@ -2,7 +2,8 @@ terraform {
     required_version = "~> 1.1"
     required_providers {
         aws = {
-            source "hashicrop/aws"
+            source = "hashicrop/aws"
+            version = "~"
         }
     }
 }
