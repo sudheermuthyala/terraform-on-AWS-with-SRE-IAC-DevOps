981297ba133e76a6fbc825424a88f9489f023eca
sudheer <sudheerlikeu@gmail.com>
2022-04-22 10:54:37 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c3-security-group.tf
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c3-security-group.tf
@@ -4,8 +4,8 @@ resource "aws_security_group" "allow_ssh" {
 
   ingress {
     description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
+    from_port        = 22
+    to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = [aws_vpc.main.cidr_block]
     ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
