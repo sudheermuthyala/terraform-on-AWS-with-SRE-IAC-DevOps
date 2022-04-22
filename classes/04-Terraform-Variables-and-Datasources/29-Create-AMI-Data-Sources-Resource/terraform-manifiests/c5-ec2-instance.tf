0ae372fba5b4cf9425ed87c86da0e5f65652a461
sudheer <sudheerlikeu@gmail.com>
2022-04-22 12:27:11 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "Myec2" {
   ami           = data.aws_ami.amazonimage
   instance_type = var.instance_type
   key_name      = var.instance_key_pair
-  user_data     = file("$")
+  user_data     = file("${path.module}")
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web.id ]
 
   tags = {
