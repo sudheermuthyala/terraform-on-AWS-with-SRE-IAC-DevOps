5e3e0688a70c7835b9066d771a394beefcf69cc5
sudheer <sudheerlikeu@gmail.com>
2022-04-22 12:37:43 +0530

    --- Auto-Git Commit ---

--- classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
+++ classes/04-Terraform-Variables-and-Datasources/29-Create-AMI-Data-Sources-Resource/terraform-manifiests/c5-ec2-instance.tf
@@ -1,7 +1,7 @@
 # resource aws_instance
 
 resource "aws_instance" "Myec2" {
-  ami           = data.aws_ami.amazonimage
+  ami           = data.aws_ami.amazonimage.id
   instance_type = var.instance_type
   key_name      = var.instance_key_pair
   user_data     = file("${path.module}/app1-install.sh")
