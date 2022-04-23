966638d950cc907521c819682b91b29974e70521
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:08:34 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
@@ -1,2 +1,11 @@
 ## Resource: aws_instance
 
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
+
