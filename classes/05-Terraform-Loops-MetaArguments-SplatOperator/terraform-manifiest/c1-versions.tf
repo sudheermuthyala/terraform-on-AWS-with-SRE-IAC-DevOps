cabab734d830359ba0469c2694dc0ea7cb02b8ee
sudheer <sudheerlikeu@gmail.com>
2022-04-23 14:52:10 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c1-versions.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c1-versions.tf
@@ -2,7 +2,6 @@ terraform {
     required_version = "~> 1.1"
     required_providers {
         aws = {
-            source = hashicorp
-        }
+            source = "hashicorp/aws"        }
     }
 }
