2d79fe5b8345fce85c5ee7c469ff18d278b84c10
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:40:05 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
@@ -3,7 +3,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.rkalluru-CO-7.id
   #instance_type = var.insyance_type                ## Default
-  #instance_type = var.instance_type_list[1]        ## List 
+  instance_type = var.instance_type_list[1]        ## List 
   #instance_type = var.insyance_type_map["prod"]    ## map
   key_name      = var.instance_key_pair
   user_data     = file("${path.module}/app1-install.sh")
