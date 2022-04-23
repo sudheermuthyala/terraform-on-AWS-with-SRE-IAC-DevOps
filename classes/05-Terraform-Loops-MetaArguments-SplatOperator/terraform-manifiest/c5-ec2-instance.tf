9a54f7bb9adec5839e2554d9b74a617e971df70a
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:16:27 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c5-ec2-instance.tf
@@ -3,6 +3,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.rkalluru-CO-7.id
   instance_type = var.insyance_type
+  key_name = 
   user_data     = file("${path.module}/app1-install.sh")
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id , aws_security_group.allow_web.id]
 
