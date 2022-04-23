6bb5d37bc6cf20a057c4c2881cc25e18c9268e1e
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:31:51 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c2-varibles.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c2-varibles.tf
@@ -22,5 +22,5 @@ variable "instance_key_pair" {
 variable "instance_type_list" {
   description = "Ec2 instance types in List"
   type = list(string)
-  default = [ "" ]
+  default = [ "t3.micro" ]
 }
