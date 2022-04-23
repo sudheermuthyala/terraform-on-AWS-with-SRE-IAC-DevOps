8ba0c4355872390ef855796bb3d99c7009f401ad
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:10:25 +0530

    --- Auto-Git Commit ---

---
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c4-ami-datasource.tf
@@ -0,0 +1,27 @@
+## Data Source: aws_ami
+data.aws_ami
+data "aws_ami" "rkaluruCO-7" {
+  most_recent      = true
+  owners           = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["Centos-7-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+  
+
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+}
