3444639822dddf1aa33b862bbb9ede239e1157e2
sudheer <sudheerlikeu@gmail.com>
2022-04-23 15:04:12 +0530

    --- Auto-Git Commit ---

---
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/terraform-manifiest/c4-ami-datasource.tf
@@ -0,0 +1,23 @@
+## Data Source: aws_ami
+
+ata "aws_ami" "example" {
+  executable_users = ["self"]
+  most_recent      = true
+  name_regex       = "^myami-\\d{3}"
+  owners           = ["self"]
+
+  filter {
+    name   = "name"
+    values = ["myami-*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
