0c5970b911040cf162e78e905b9edf0a272d22ae
sudheer <sudheerlikeu@gmail.com>
2022-04-25 11:29:58 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami           = data.aws_ami.my_rk_ami.id
   instance_type = var.instance_type
-  key_name
+  key_name      = var.
 
   tags = {
     Name = "HelloWorld"
