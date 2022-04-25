0c7d0e83b49f14dc7e93e199eb02c335cb616979
sudheer <sudheerlikeu@gmail.com>
2022-04-25 15:18:30 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
@@ -5,7 +5,7 @@ resource "aws_instance" "myec2vm" {
   #instance_type             =  var.instance_type_map["prod"]
   key_name                  = var.key_name
   user_data                 = file("${path.module}/app1-install.sh")
-  vpc_security_group_ids    =     [ aws_security_group.allow_ssh.id , aws_security_group.allow_web.id ]
+  vpc_security_group_ids    = [ aws_security_group.allow_ssh.id , aws_security_group.allow_web.id ]
   count = 2
 
 
