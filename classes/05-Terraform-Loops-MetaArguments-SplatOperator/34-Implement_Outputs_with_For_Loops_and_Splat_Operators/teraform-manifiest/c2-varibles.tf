65f1ff5893dc4cb7f133c6f64daacb45b9e62c76
sudheer <sudheerlikeu@gmail.com>
2022-04-25 11:40:51 +0530

    --- Auto-Git Commit ---

---
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c2-varibles.tf
@@ -0,0 +1,30 @@
+## aws_region
+variable "aws_region" {
+  description = "aws region launch the servers"
+  type = string
+  default = "us-east-1"
+}
+
+## instance_type
+
+variable "instance_type" {
+  description = "instance type"
+  type = string
+  default = "t2.micro"
+}
+## key_name
+
+variable "key_name" {
+  description = "instance key name"
+  type = string
+  default = "terraform-k"
+}
+
+## varibles for instance type  list
+
+variable "instance_type_list" {
+    description = "ec2 instance type list"
+    type = list(string)
+    default = [ "t2.micro", ""]
+  
+}
