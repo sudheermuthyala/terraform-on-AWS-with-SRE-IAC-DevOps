618a952bf75d2c2cc70a698c099be50f9b39646f
sudheer <sudheerlikeu@gmail.com>
2022-04-25 11:49:53 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c5-ec2-instance.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web" {
   ami                       = data.aws_ami.my_rk_ami.id
   #instance_type             = var.instance_type
-  ins
+  instance_type             = 
   key_name                  = var.key_name
   user_data                 = file("${path.module}/app1-install.sh")
   vpc_security_group_ids    =     [ aws_security_group.allow_ssh.id , aws_security_group.allow_web.id ]
