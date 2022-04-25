23e43e628f1a185d029652b3f57eff73d204d4e2
sudheer <sudheerlikeu@gmail.com>
2022-04-25 11:16:23 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c1-versions.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c1-versions.tf
@@ -2,7 +2,8 @@ terraform {
     required_version = "~> 1.1"
     required_providers {
         aws = {
-            version = "~> 4 "
+            version = "~> 4"
+            source  = 
         }
     }
 }
