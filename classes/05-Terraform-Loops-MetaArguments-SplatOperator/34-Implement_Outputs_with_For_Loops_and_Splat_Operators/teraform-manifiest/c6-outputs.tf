e265181855a276f8d969512598d6e258d3bb829a
sudheer <sudheerlikeu@gmail.com>
2022-04-25 12:32:36 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c6-outputs.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/34-Implement_Outputs_with_For_Loops_and_Splat_Operators/teraform-manifiest/c6-outputs.tf
@@ -3,4 +3,6 @@
 2. for loop with map
 3. for loop with map Advanced
 4. Legacy Splat Operator (Latest) - Returns List
-5. Legacy Generalized Splat Operator
+5. Legacy Generalized Splat Operator returens the List
+
+
