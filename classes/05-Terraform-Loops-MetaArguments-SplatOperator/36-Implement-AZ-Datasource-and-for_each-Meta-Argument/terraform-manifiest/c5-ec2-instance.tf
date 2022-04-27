d3a4c71a6cfc60acc0558ee1742e5a2bac30d36a
sudheer <sudheerlikeu@gmail.com>
2022-04-27 11:51:45 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -1,8 +1,8 @@
 resource "aws_instance" "myec2vm" {
   ami         =  data.aws_ami.amazonimage.id
   #instance_type = var.instance_type
-  instance_type = var.instance_type_list[1]
-  instance_type = var.instance_type_list[1]
+  #instance_type = var.instance_type_list[1]
+  instance_type = var.instance_type_list[0]
   
 
 }
