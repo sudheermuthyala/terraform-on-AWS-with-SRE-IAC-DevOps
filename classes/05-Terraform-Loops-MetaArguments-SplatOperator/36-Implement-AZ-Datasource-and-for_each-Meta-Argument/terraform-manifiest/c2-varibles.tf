477b46562d9b20747065093a0ea633e6e772e0dc
sudheer <sudheerlikeu@gmail.com>
2022-04-27 11:48:54 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c2-varibles.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c2-varibles.tf
@@ -22,6 +22,9 @@ variable "instance_type_list" {
 
 variable "instance_type_mape" {
     description = "Mape of varibles for instance type"
-    type =  map()
+    type =  map(string)
+    default = {
+      "dev" = "value"
+    }
   
 }
