3fc610e0a9b3cc54828c9a100f713f91fd04e25d
sudheer <sudheerlikeu@gmail.com>
2022-04-27 11:39:31 +0530

    --- Auto-Git Commit ---

---
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -0,0 +1,15 @@
+resource "aws_instance" "myec2vm" {
+  ami                       = data.aws_ami.amazonimage.id
+  #instance_type             = var.instance_type
+  instance_type             = var.instance_type_list[1]
+  #instance_type             =  var.instance_type_map["prod"]
+  key_name                  = var.key_name
+  user_data                 = file("${path.module}/app1-install.sh")
+  vpc_security_group_ids    = [ aws_security_group.allow_ssh.id , aws_security_group.allow_web.id ]
+  count = 2
+
+
+  tags = {
+    Name = "Demo-${count.index}"
+  }
+}
