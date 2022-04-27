c3065465c1c3db8c35b44d1366b78c682ca219d6
sudheer <sudheerlikeu@gmail.com>
2022-04-27 11:56:16 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -4,7 +4,8 @@ resource "aws_instance" "myec2vm" {
   #instance_type = var.instance_type_list[1]
   instance_type = var.instance_type_map["prod"]
   key_name = var.key_name
-  vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web ]
+  vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web.id ]
+  user_data = 
 
   
 
