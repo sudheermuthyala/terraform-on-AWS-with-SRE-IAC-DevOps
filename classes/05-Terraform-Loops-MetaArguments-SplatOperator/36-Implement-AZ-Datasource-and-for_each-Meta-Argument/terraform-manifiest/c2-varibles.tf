c84aadf46b00f164be1a74b709d4f468cfd5136e
sudheer <sudheerlikeu@gmail.com>
2022-04-27 11:41:52 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c2-varibles.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c2-varibles.tf
@@ -3,3 +3,7 @@ variable "aws_region" {
     type = string   
     default = "us-east-1"
 }
+
+variable "instance_type" {
+  
+}
--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -1,4 +1,4 @@
 resource "aws_instance" "myec2vm" {
   ami         =  data.aws_ami.amazonimage.id
-  instance_type = var
+  instance_type = var.instance_type
 }
