c3665177b43a167d88d5af482018fe3b7670642f
sudheer <sudheerlikeu@gmail.com>
2022-04-28 10:04:10 +0530

    --- Auto-Git Commit ---

---
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
@@ -0,0 +1,12 @@
+output "for_loop_with_list" {
+    description = "for_loop_with_list"
+    value =  [ for instance in aws_instance.myec2vm: instance.public_dns]
+  
+}
+
+
+output "for_loop_with_list" {
+    description = "for loop with map"
+    value = {for instance in aws_instance.myec2vm:     }
+  
+}
