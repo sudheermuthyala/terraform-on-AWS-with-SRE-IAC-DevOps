536eb7bb405fa28f039f7bac88f9dd5b9be1d61c
sudheer <sudheerlikeu@gmail.com>
2022-04-28 10:41:31 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -1,13 +1,10 @@
 ##  Data Source: aws_availability_zones
 data "aws_availability_zones" "example" {
-  all_availability_zones = true
-
   filter {
     name   = "opt-in-status"
-    values = ["not-opted-in", "opted-in"]
+    values = ["opt-in-not-required"]
   }
 }
-
 
 
 
