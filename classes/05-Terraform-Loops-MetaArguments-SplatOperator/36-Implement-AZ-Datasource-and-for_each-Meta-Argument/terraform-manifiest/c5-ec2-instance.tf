15152e7c8aa85fe60994fa11d95faa4a7fdcea1c
sudheer <sudheerlikeu@gmail.com>
2022-04-28 10:41:11 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -8,6 +8,12 @@ data "aws_availability_zones" "example" {
   }
 }
 
+data "aws_availability_zones" "example" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
 
 
 
