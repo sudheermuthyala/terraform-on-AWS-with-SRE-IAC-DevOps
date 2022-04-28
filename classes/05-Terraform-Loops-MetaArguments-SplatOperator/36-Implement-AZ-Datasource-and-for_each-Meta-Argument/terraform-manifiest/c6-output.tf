6705df4c617799e7d513736bbce8cab241452f74
sudheer <sudheerlikeu@gmail.com>
2022-04-28 10:12:42 +0530

    --- Auto-Git Commit ---

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
@@ -1,3 +1,6 @@
+
+## Output for loop with List
+# while we dealing with List we provide only ["value"]
 output "for_loop_with_list" {
     description = "for_loop_with_list"
     value =  [ for instance in aws_instance.myec2vm: instance.public_dns]
