c646214a32a916bfa94ee2dd21a1483212368bd1
sudheer <sudheerlikeu@gmail.com>
2022-05-09 12:41:27 +0530

    terraform practice

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -12,8 +12,8 @@ data "aws_availability_zones" "my_AZ_zones" {
 resource "aws_instance" "myec2vm" {
   #count       =   2
   ami         =  data.aws_ami.amazonimage.id
-  instance_type = var.instance_type
-  #instance_type = var.instance_type_list[1]
+  #instance_type = var.instance_type
+  instance_type = var.instance_type_list[1]
   #instance_type = var.instance_type_map["prod"]
   key_name = var.key_name
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web.id ]
