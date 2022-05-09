9cfc2c65c4501f205626a33e98506e12c62e809f
sudheer <sudheerlikeu@gmail.com>
2022-05-09 13:12:56 +0530

    terraform practice

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
@@ -21,22 +21,22 @@ output "for_loop_with_map_2" {
   value = {for c, instance in aws_instance.myec2vm: c=> instance.public_dns}
 }
 
-
+# NOTE: This will not work when using fro_each [Meta Aurgement]
 # output Legacy Splat Operator (Legacy) - Returns List
 ## We are going to comment the legacy splat operator, which might be decommissioned in future versions
-output "Legacy_Splat_Operator" {
-    description = "Legacy splat Operator"
-    value = aws_instance.myec2vm.*.public_dns
+# output "Legacy_Splat_Operator" {
+    # description = "Legacy splat Operator"
+    # value = aws_instance.myec2vm.*.public_dns
   
-}
+# }
 
 # Output Latest Generalized Splat Operator - Returns the List
 
-output "Latest_Generalized_Splat_Operator" {
-    description = " latest Generalized Operator"
-    value = aws_instance.myec2vm[*].public_dns
-  
-}
+#  output "Latest_Generalized_Splat_Operator" {
+    # description = " latest Generalized Operator"
+    # value = aws_instance.myec2vm[*].public_dns
+#   
+# }
 
 # to view the list of aws_availability_zones  Names in the region  
 output "AZ" {
