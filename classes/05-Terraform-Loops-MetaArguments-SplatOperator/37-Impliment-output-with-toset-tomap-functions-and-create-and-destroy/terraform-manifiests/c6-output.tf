e96eb6db604a1f972e108afc4d7ea9c128079e47
sudheer <sudheerlikeu@gmail.com>
2022-05-09 15:32:58 +0530

    terraform practice

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/37-Impliment-output-with-toset-tomap-functions-and-create-and-destroy/terraform-manifiests/c6-output.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/37-Impliment-output-with-toset-tomap-functions-and-create-and-destroy/terraform-manifiests/c6-output.tf
@@ -4,3 +4,8 @@ output "for_loop_with_list" {
   description = "for_loop_with_list"
   value = [for instance in aws_instance.myec2vm: instance.public_dns]
 }
+
+output "for_loop_with_map" {
+  description = "for_loop_with_map"
+  value = [for AZ, Instance in aws_instance.myec2vm: AZ => instance.public_ip]
+}
