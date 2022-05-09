57c41a42fe187a44e0d6eea043f39e354c464a61
sudheer <sudheerlikeu@gmail.com>
2022-05-09 13:38:43 +0530

    terraform practice

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c6-output.tf
@@ -18,7 +18,7 @@ output "for_loop_with_map_1" {
 # Output - For Loop with Map Advanced
 output "for_loop_with_map_2" {
   description = "for loop with map-2"
-  value = {for c, instance in aws_instance.myec2vm: c=> instance.public_dns}
+  value = {for AZ, instance in aws_instance.myec2vm: AZ=> instance.public_dns}
 }
 
 # NOTE: This will not work when using fro_each [Meta Aurgement]
