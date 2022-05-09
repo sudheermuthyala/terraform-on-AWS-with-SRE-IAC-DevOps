321e6f9da94f3c0f8f4948e0cfb484eed2e0a02a
sudheer <sudheerlikeu@gmail.com>
2022-05-09 12:33:46 +0530

    terraform practice

--- classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
+++ classes/05-Terraform-Loops-MetaArguments-SplatOperator/36-Implement-AZ-Datasource-and-for_each-Meta-Argument/terraform-manifiest/c5-ec2-instance.tf
@@ -13,8 +13,8 @@ resource "aws_instance" "myec2vm" {
   #count       =   2
   ami         =  data.aws_ami.amazonimage.id
   #instance_type = var.instance_type
-  #instance_type = var.instance_type_list[1]
-  instance_type = var.instance_type_map["prod"]
+  instance_type = var.instance_type_list[1]
+  #instance_type = var.instance_type_map["prod"]
   key_name = var.key_name
   vpc_security_group_ids = [ aws_security_group.allow_ssh.id, aws_security_group.allow_web.id ]
   user_data = file("${path.module}/app1-install.sh")
