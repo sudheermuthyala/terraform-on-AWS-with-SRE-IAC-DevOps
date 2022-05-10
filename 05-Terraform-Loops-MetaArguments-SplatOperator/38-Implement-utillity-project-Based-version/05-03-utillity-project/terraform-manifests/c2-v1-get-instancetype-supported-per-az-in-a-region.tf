0aebfcf482904a2fc0b36aafe68051c9696e73db
sudheer <sudheerlikeu@gmail.com>
2022-05-10 14:56:12 +0530

    terraform practice

--- 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,6 +1,7 @@
 ## c2-v1-get-instancetype-supported-per-az-in-a-region.tf
 
 ## Datasource aws_ec2_instance_type_offerings
+## Review / Create the datasource and its output
 
 data "aws_ec2_instance_type_offerings" "my_inst_type1" {
   filter {
---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -0,0 +1,22 @@
+## Using for_each create multiple instances of datasource and loop it with hard-coded availability zones in for_each
+## Review / Create the datasource and its output with for_each
+
+data "aws_ec2_instance_type_offerings" "my_inst_type2" {
+    for_each    = toset(["us-east-1a","us-east-1b","us-east-1e"])
+  filter {
+    name   = "instance-type"
+    values = ["t3.micro"]
+  }
+
+  filter {
+    name   = "location"
+    values = [each.key]
+  }
+
+  location_type = "availability-zone"
+}
+
+
+output "output_v2_1" {
+  value = [for t in data.aws_ec2_instance_type_offerings.my_inst_type2.instance_types: t.instance_types]
+}
