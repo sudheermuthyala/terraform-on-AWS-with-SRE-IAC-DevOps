7aca045207ca2f60575471323d8e8ea6c872c773
sudheer <sudheerlikeu@gmail.com>
2022-05-10 15:49:11 +0530

    terraform practice

--- 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v1-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,4 +1,4 @@
-## c2-v1-get-instancetype-supported-per-az-in-a-region.tf
+/*## c2-v1-get-instancetype-supported-per-az-in-a-region.tf
 
 ## Datasource aws_ec2_instance_type_offerings
 ## Review / Create the datasource and its output
@@ -28,3 +28,5 @@ data "aws_ec2_instance_type_offerings" "my_inst_type1" {
 output "output_v1_1_us_east_1e" {
     value = data.aws_ec2_instance_type_offerings.my_inst_type1.instance_types
 }
+
+*/
--- 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -1,3 +1,4 @@
+/*
 ## Using for_each create multiple instances of datasource and loop it with hard-coded availability zones in for_each
 ## Review / Create the datasource and its output with for_each
 
@@ -28,3 +29,5 @@ output "output_v2_2_Map" {
       for az, details in data.aws_ec2_instance_type_offerings.my_inst_type2: az => details.instance_types
   }
 }
+
+*/
