cafeca4ea0ef80600a6d9dac9637909aff3c146a
sudheer <sudheerlikeu@gmail.com>
2022-05-10 15:35:06 +0530

    terraform practice

---
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v3-get-instancetype-supported-per-az-in-a-region.tf
@@ -0,0 +1,27 @@
+## Datasource-1
+data "aws_availability_zones" "my_AZ_zones" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
+## Datasource-2
+data "aws_ec2_instance_type_offerings" "my_inst_type" {
+    for_each    = toset([data.aws_availability_zones.my_AZ_zones.names])
+  filter {
+    name   = "instance-type"
+    values = ["t3.micro"]
+  }
+
+  filter {
+    name   = "location"
+    values = [each.key]
+  }
+
+  location_type = "availability-zone"
+}
+
+output "output_v3_1" {
+  value = [for t in aws_ec2_instance_type_offerings.my_inst_type: t.instance_types]
+}
