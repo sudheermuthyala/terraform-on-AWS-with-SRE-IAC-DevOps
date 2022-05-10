57c0522564c2f1bda0d5be46fda55fb7bdd7cc1d
sudheer <sudheerlikeu@gmail.com>
2022-05-10 15:05:09 +0530

    terraform practice

--- 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
+++ 05-Terraform-Loops-MetaArguments-SplatOperator/38-Implement-utillity-project-Based-version/05-03-utillity-project/terraform-manifests/c2-v2-get-instancetype-supported-per-az-in-a-region.tf
@@ -20,3 +20,10 @@ data "aws_ec2_instance_type_offerings" "my_inst_type2" {
 output "output_v2_1" {
   value = [for t in data.aws_ec2_instance_type_offerings.my_inst_type2: t.instance_types]
 }
+
+
+output "output_v2_2" {
+  value = {
+      for az, details in data.aws_ec2_instance_type_offerings.my_inst_type2: az => details.instance_types
+  }
+}
